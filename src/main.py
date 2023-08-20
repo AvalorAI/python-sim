8574097ddb180f5555121419a2c6a38a09ca2128
MarcD12345 <marc@derksen.me>
2023-08-20 16:43:57 +0200

    enhanced simulator

--- src/main.py
+++ src/main.py
@@ -1,251 +1,286 @@
-import random
-import math
+import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import gridspec
+from mpl_toolkits.mplot3d import Axes3D
+import csv
 
-class Drone:
-    def __init__(self):
-        # Drone properties
-        self.mass = 1.0
-        self.thrust_coefficient = 0.1
-        self.drag_coefficient = 0.01
+# Constants
+g = 9.81
+m = 1.0
+l = 0.5
+k = 4.905
+kd = 0.1
 
-        # State variables
-        self.position = [0.0, 0.0, 0.0]
-        self.velocity = [0.0, 0.0, 0.0]
-        self.acceleration = [0.0, 0.0, 0.0]
-        self.orientation = [0.0, 0.0, 0.0]
-        self.angular_velocity = [0.0, 0.0, 0.0]
-        self.angular_acceleration = [0.0, 0.0, 0.0]
+# Initialize State and History for Visualization
+state = {
+    'x': 0,
+    'y': 0,
+    'z': 0,
+    'v_x': 0,
+    'v_y': 0,
+    'v_z': 0,
+    'roll': 0,
+    'pitch': 0,
+    'yaw': 0,
+    'angular_v': np.array([0, 0, 0])
+}
 
-        # Control variables
-        self.actuator_outputs = [0.0, 0.0, 0.0, 0.0]  # [front-left, front-right, rear-left, rear-right]
+history = {key: [] for key in state.keys()}
+actuator_history = []
+force_history = []
 
-    def apply_thrust(self, actuator_outputs):
-        self.actuator_outputs = actuator_outputs
+def forces_from_actuators(actuators):
+    f_total = sum(actuators)
+    torques = np.array([
+        (actuators[1] - actuators[3]) * l * k, # Roll
+        (actuators[0] - actuators[2]) * l * k, # Pitch
+        (actuators[0] + actuators[2] - actuators[1] - actuators[3]) * kd # Yaw
+    ])
+    return f_total, torques
 
-    def compute_drag(self):
-        velocity_magnitude = math.sqrt(sum(v ** 2 for v in self.velocity))
-        drag_force = [-self.drag_coefficient * v * velocity_magnitude for v in self.velocity]
-        return drag_force
+def rotation_matrix(roll, pitch, yaw):
+    cr = np.cos(roll)
+    sr = np.sin(roll)
+    cp = np.cos(pitch)
+    sp = np.sin(pitch)
+    cy = np.cos(yaw)
+    sy = np.sin(yaw)
 
-    def compute_gravity(self):
-        gravity_force = [0.0, 0.0, -self.mass * 9.81]
-        return gravity_force
-
-    def compute_net_force(self):
-        total_thrust_magnitude = sum([self.thrust_coefficient * output for output in self.actuator_outputs])
-        total_drag = self.compute_drag()
-        total_gravity = self.compute_gravity()
-
-        # Decompose thrust based on orientation (pitch, roll, yaw) and the new actuator setup
-        thrust_x = total_thrust_magnitude * math.sin(self.orientation[1])  # sin(pitch)
-        thrust_y = -total_thrust_magnitude * math.sin(self.orientation[0])  # -sin(roll)
-        thrust_z = total_thrust_magnitude * math.cos(self.orientation[0]) * math.cos(self.orientation[1])  # cos(roll)*cos(pitch)
-
-        
-        net_force_x = thrust_x + total_drag[0]
-        net_force_y = thrust_y + total_drag[1]
-        net_force_z = thrust_z + total_drag[2] + total_gravity[2]
-        
-        return [net_force_x, net_force_y, net_force_z]
-
-    def angular_accelerations(self):
-        
-        roll_diff = (self.actuator_outputs[1] - self.actuator_outputs[0]) + (self.actuator_outputs[3] - self.actuator_outputs[2])
-        pitch_diff = (self.actuator_outputs[0] + self.actuator_outputs[1]) - (self.actuator_outputs[2] + self.actuator_outputs[3])
-        yaw_diff = (self.actuator_outputs[0] + self.actuator_outputs[3]) - (self.actuator_outputs[1] + self.actuator_outputs[2])
-
-        roll_acceleration = roll_diff / self.mass
-        pitch_acceleration = pitch_diff / self.mass
-        yaw_acceleration = yaw_diff / self.mass
-
-        return [roll_acceleration, pitch_acceleration, yaw_acceleration]
-
-    def update_acceleration(self):
-        self.acceleration = [force / self.mass for force in self.compute_net_force()]
-
-    def update_velocity(self, dt):
-        self.velocity = [v + a * dt for v, a in zip(self.velocity, self.acceleration)]
-
-    def update_position(self, dt):
-        self.position = [p + v * dt for p, v in zip(self.position, self.velocity)]
-
-    def update_angular_acceleration(self):
-        self.angular_acceleration = self.angular_accelerations()
-
-    def update_angular_velocity(self, dt):
-        self.angular_velocity = [w + alpha * dt for w, alpha in zip(self.angular_velocity, self.angular_acceleration)]
-
-    def update_orientation(self, dt):
-        self.orientation = [angle + omega * dt for angle, omega in zip(self.orientation, self.angular_velocity)]
-
-    def compute_forward_vector(self, pitch, yaw, roll):
-        pitch = math.radians(pitch)
-        yaw = math.radians(yaw)
-        
-        fx = math.sin(yaw) * math.cos(pitch)
-        fy = -math.cos(yaw) * math.cos(pitch)
-        fz = math.sin(pitch)
-        
-        return fx, fy, fz
-
-    def simulate(self, dt, actuator_outputs):
-        self.apply_thrust(actuator_outputs)
-        self.update_acceleration()
-        self.update_velocity(dt)
-        self.update_position(dt)
-
-        self.update_angular_acceleration()
-        self.update_angular_velocity(dt)
-        self.update_orientation(dt)
-
-        # Print position and orientation for debugging
-        print(f"Time: {round(dt, 2)}s")
-        print(f"Position: X={round(self.position[0], 2)}m, Y={round(self.position[1], 2)}m, Z={round(self.position[2], 2)}m")
-        print(f"Orientation: Roll={round(self.orientation[0], 2)}°, Pitch={round(self.orientation[1], 2)}°, Yaw={round(self.orientation[2], 2)}°")
-        print("-----")
-
-    def simulate_flight(self, actuator_sequences, durations):
-        dt = 0.01
-        total_time = sum(durations)
-        num_steps = int(total_time / dt)
-        x_data = []
-        y_data = []
-        z_data = []
-        roll_data = []
-        pitch_data = []
-        yaw_data = []
-        elapsed_time = 0
-
-        vx_data = []
-        vy_data = []
-        vz_data = []
-        omega_roll_data = []
-        omega_pitch_data = []
-        omega_yaw_data = []
-        alpha_roll_data = []
-        alpha_pitch_data = []
-        alpha_yaw_data = []
-
-        for step in range(num_steps):
-            # Update actuator outputs based on elapsed time
-            accumulated_duration = 0
-            for idx, duration in enumerate(durations):
-                accumulated_duration += duration
-                if elapsed_time < accumulated_duration:
-                    self.simulate(dt, actuator_sequences[idx])
-                    break
-
-            x_data.append(self.position[0])
-            y_data.append(self.position[1])
-            z_data.append(self.position[2])
-            roll_data.append(self.orientation[0])
-            pitch_data.append(self.orientation[1])
-            yaw_data.append(self.orientation[2])
-            elapsed_time += dt
-
-            vx_data.append(self.velocity[0])
-            vy_data.append(self.velocity[1])
-            vz_data.append(self.velocity[2])
-            omega_roll_data.append(self.angular_velocity[0])
-            omega_pitch_data.append(self.angular_velocity[1])
-            omega_yaw_data.append(self.angular_velocity[2])
-            alpha_roll_data.append(self.angular_acceleration[0])
-            alpha_pitch_data.append(self.angular_acceleration[1])
-            alpha_yaw_data.append(self.angular_acceleration[2])
-
-        return x_data, y_data, z_data, roll_data, pitch_data, yaw_data, vx_data, vy_data, vz_data, omega_roll_data, omega_pitch_data, omega_yaw_data, alpha_roll_data, alpha_pitch_data, alpha_yaw_data
-
-
-def main():
-    # Create a drone instance
-    drone = Drone()
-
-# Define simulation parameters
-    simulation_time = 20  # seconds
+    R = np.array([
+        [cy*cp, cy*sp*sr-sy*cr, cy*sp*cr+sy*sr],
+        [sy*cp, sy*sp*sr+cy*cr, sy*sp*cr-cy*sr],
+        [-sp, cp*sr, cp*cr]
+    ])
     
-    # Assuming position data every 0.01 seconds
-    time_data = [i*0.01 for i in range(int(simulation_time/0.01))]
+    return R
 
-    hover_thrust_per_motor = 2.5 * drone.mass * 9.81  # Adjusted thrust for stability
-    neutral_thrust_per_motor = 0
-
-    # Define actuator outputs for each phase
-    ascent_outputs = [hover_thrust_per_motor * 1.1] * 4  
-    hover_outputs = [hover_thrust_per_motor] * 4
-    descent_outputs = [hover_thrust_per_motor * 0.9] * 4
-
-    # Create actuator sequences and durations lists
-    actuator_sequences = [ascent_outputs, [20,10,0,50], ascent_outputs, [0,0,0,0]]
-    durations = [5, 5, 5, 5]  # Assuming each phase lasts 5 seconds
-
-    # Simulate the flight and get position and orientation data
-    x_data, y_data, z_data, roll_data, pitch_data, yaw_data, vx_data, vy_data, vz_data, omega_roll_data, omega_pitch_data, omega_yaw_data, alpha_roll_data, alpha_pitch_data, alpha_yaw_data = drone.simulate_flight(actuator_sequences, durations)
-
-
-    # Create a GridSpec layout
-    fig = plt.figure(figsize=(12, 10))
-    gs = gridspec.GridSpec(4, 2, width_ratios=[2, 1])
-
-    # 3D plot for drone's movement
-    ax1 = fig.add_subplot(gs[0, 0], projection='3d')
-    ax1.plot(x_data, y_data, z_data)
-
-    ax1.set_xlabel('X Position (m)')
-    ax1.set_ylabel('Y Position (m)')
-    ax1.set_zlabel('Altitude (m)')
-    ax1.set_title('Quadcopter 3D Flight Path')
-
-    # Altitude plot
-    ax5 = fig.add_subplot(gs[0, 1])
-    ax5.plot(time_data, z_data)
-    ax5.set_xlabel('Time (s)')
-    ax5.set_ylabel('Altitude (m)')
-    ax5.set_title('Drone Altitude over Time')
-
-    # Orientation data plots
-    ax2 = fig.add_subplot(gs[1, 0], sharex=ax5)
-    ax2.plot(time_data, roll_data)
-    ax2.set_ylabel('Roll (°)')
-    ax2.set_title('Roll Orientation over Time')
-
-    ax3 = fig.add_subplot(gs[1, 1], sharex=ax5)
-    ax3.plot(time_data, pitch_data)
-    ax3.set_ylabel('Pitch (°)')
-    ax3.set_title('Pitch Orientation over Time')
-
-    ax4 = fig.add_subplot(gs[2, 0], sharex=ax5)  # Use both columns for the yaw data
-    ax4.plot(time_data, yaw_data)
-    ax4.set_ylabel('Yaw (°)')
-    ax4.set_title('Yaw Orientation over Time')
-
-    ax6 = fig.add_subplot(gs[2, 1])
-    ax6.plot(time_data, vx_data, label="Vx")
-    ax6.plot(time_data, vy_data, label="Vy")
-    ax6.plot(time_data, vz_data, label="Vz")
-    ax6.set_ylabel('Velocity (m/s)')
-    ax6.set_title('Linear Velocity over Time')
-    ax6.legend()
-
-    ax7 = fig.add_subplot(gs[3, 0])
-    ax7.plot(time_data, omega_roll_data, label="Roll")
-    ax7.plot(time_data, omega_pitch_data, label="Pitch")
-    ax7.plot(time_data, omega_yaw_data, label="Yaw")
-    ax7.set_ylabel('Angular Velocity (rad/s)')
-    ax7.set_title('Angular Velocity over Time')
-    ax7.legend()
-
-    ax8 = fig.add_subplot(gs[3, 1])
-    ax8.plot(time_data, alpha_roll_data, label="Roll")
-    ax8.plot(time_data, alpha_pitch_data, label="Pitch")
-    ax8.plot(time_data, alpha_yaw_data, label="Yaw")
-    ax8.set_ylabel('Angular Acceleration (rad/s^2)')
-    ax8.set_title('Angular Acceleration over Time')
-    ax8.legend()
-
-    plt.tight_layout()
-    plt.show()
+def dynamics(state, actuators):
+    f, torques = forces_from_actuators(actuators)
     
-if __name__ == "__main__":
-    main()
+    # Compute the world frame thrust components
+    R = rotation_matrix(state['roll'], state['pitch'], state['yaw'])
+    world_thrust = R @ np.array([0, 0, f*k])
+    
+    dx = state['v_x']
+    dy = state['v_y']
+    dz = state['v_z']
+    
+    dvx = world_thrust[0] / m
+    dvy = world_thrust[1] / m
+    dvz = world_thrust[2] / m - g
+    
+    # Moment of inertia (for simplicity, assuming it's the same for all axes)
+    I_xx = I_yy = I_zz = 0.02  # This is just a placeholder, adjust based on your quadcopter's properties
+
+    # Angular accelerations
+    alpha_x = torques[0] / I_xx
+    alpha_y = torques[1] / I_yy
+    alpha_z = torques[2] / I_zz
+
+    # Rate of change of roll, pitch, and yaw
+    droll = state['angular_v'][0]
+    dpitch = state['angular_v'][1]
+    dyaw = state['angular_v'][2]
+
+    # Updating angular velocities
+    dangular_v = np.array([alpha_x, alpha_y, alpha_z])
+    
+    return {
+        'x': dx, 
+        'y': dy, 
+        'z': dz,
+        'v_x': dvx, 
+        'v_y': dvy, 
+        'v_z': dvz,
+        'roll': droll,
+        'pitch': dpitch,
+        'yaw': dyaw,
+        'angular_v': dangular_v
+    }
+
+def update_state_rk4(state, actuators, dt):
+    k1 = dynamics(state, actuators)
+    k2 = dynamics({key: state[key] + 0.5 * dt * k1[key] for key in state.keys()}, actuators)
+    k3 = dynamics({key: state[key] + 0.5 * dt * k2[key] for key in state.keys()}, actuators)
+    k4 = dynamics({key: state[key] + dt * k3[key] for key in state.keys()}, actuators)
+    
+    new_state = {}
+    for key in state.keys():
+        new_state[key] = state[key] + dt * (k1[key] + 2 * k2[key] + 2 * k3[key] + k4[key]) / 6
+    return new_state
+
+
+def save_flight_data_to_csv(history, filename="flight_data.csv"):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = list(history.keys())  # gets all the keys (e.g., 'x', 'y', 'z', ...)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for i in range(len(history['x'])):
+            row_data = {key: history[key][i] for key in history}
+            writer.writerow(row_data)
+
+# Constants
+dt = 0.01
+duration_per_phase = 200  # For example, each phase lasts for 10 seconds.
+
+# Actuator configurations F, L, B, R
+take_off = [0.6, 0.6, 0.6, 0.6]
+hover = [0.5, 0.5, 0.5, 0.5]
+forward_flight = [0.589, 0.59, 0.591, 0.59]
+roll = [0.5, 0.6, 0.5, 0.5]
+brake =  [0.591, 0.59, 0.589, 0.59]
+heading_change = [0.48, 0.5, 0.52, 0.5]  
+descent = [0.4, 0.4, 0.4, 0.4]
+
+phases = ['take_off', 'descent', 'hover', 'roll']
+actuator_configs = {
+    'take_off': take_off,
+    'hover': hover,
+    'forward_flight': forward_flight,
+    'heading_change': heading_change,
+    'descent': descent,
+    'brake' : brake,
+    'roll': roll
+}
+
+# Create an initial 3D plot for visualization
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+line, = ax.plot([], [], [], lw=2)
+ax.set_xlim([-5, 5])
+ax.set_ylim([-5, 5])
+ax.set_zlim([0, 10])
+
+def update_3D_trajectory(x, y, z):
+    line.set_data(x, y)
+    line.set_3d_properties(z)
+    plt.draw()
+    plt.pause(0.01)
+
+# Simulation loop
+for phase in phases:
+    actuators = actuator_configs[phase]
+    for _ in range(duration_per_phase):
+        actuator_history.append(actuators)
+        f, torques = forces_from_actuators(actuators)
+        force_history.append((f, torques))
+        state = update_state_rk4(state, actuators, dt)
+        for key in state.keys():
+            history[key].append(state[key])
+        
+        # Update the live 3D plot with the new position
+        update_3D_trajectory(history['x'], history['y'], history['z'])
+
+plt.show()
+
+
+# Compute acceleration (difference in velocity over dt)
+acceleration_z = np.diff(history['v_z']) / dt
+# Pad with 0 at the beginning to make it the same length as history
+acceleration_z = np.insert(acceleration_z, 0, 0)
+
+# Compute angular acceleration (difference in angular velocity over dt for each axis)
+angular_acceleration = (np.diff(np.array(history['angular_v']), axis=0) / dt).tolist()
+# Pad with 0s at the beginning to make it the same length as history
+angular_acceleration = [[0,0,0]] + angular_acceleration
+
+plt.figure(figsize=(15, 35))
+
+# Position
+
+# # X distance
+# plt.subplot(7, 2, 1)  # Adjusted row count to 6
+# plt.plot(history['x'])
+# plt.title("X Distance over Time")
+# plt.ylabel("X Distance (m)")
+
+# # Y distance
+# plt.subplot(7, 2, 2)  # Adjusted row count to 6
+# plt.plot(history['y'])
+# plt.title("Y Distance over Time")
+# plt.ylabel("Y Distance (m)")
+
+# Z distance
+plt.subplot(7, 2, 3)
+plt.plot(history['z'])
+plt.title("Altitude over Time")
+plt.ylabel("Altitude (m)")
+
+# Velocity
+
+# v_x
+plt.subplot(7, 2, 4)
+plt.plot(history['v_x'])
+plt.title("Vx over Time")
+plt.ylabel("Vx (m/s)")
+
+# v_y
+plt.subplot(7, 2, 5)
+plt.plot(history['v_y'])
+plt.title("Vy over Time")
+plt.ylabel("Vy (m/s)")
+
+# v_z 
+plt.subplot(7, 2, 6)
+plt.plot(history['v_z'])
+plt.title("Vz over Time")
+plt.ylabel("Vz (m/s)")
+
+
+# Acceleration
+
+plt.subplot(7, 2, 13)
+plt.plot(acceleration_z)
+plt.title("Acceleration in Z over Time")
+plt.ylabel("Acceleration (m/s^2)")
+
+# Attitude
+
+# Roll
+plt.subplot(7, 2, 7)
+plt.plot(history['roll'])
+plt.title("Roll Angle over Time")
+plt.ylabel("Roll (radians)")
+
+# Pitch
+plt.subplot(7, 2, 8)
+plt.plot(history['pitch'])
+plt.title("Pitch Angle over Time")
+plt.ylabel("Pitch (radians)")
+
+# Yaw
+plt.subplot(7, 2, 9)
+plt.plot(history['yaw'])
+plt.title("Yaw Angle over Time")
+plt.ylabel("Yaw (radians)")
+
+# Angular Velocity
+plt.subplot(7, 2, 10)
+labels = ['X', 'Y', 'Z']
+for i in range(3):
+    plt.plot([v[i] for v in history['angular_v']], label=f'Angular Vel. {labels[i]}')
+plt.title("Angular Velocities over Time")
+plt.ylabel("Ang. Vel. (rad/s)")
+plt.legend()
+
+# Actuator Outputs and Forces
+plt.subplot(7, 2, 11)
+for i in range(4):
+    plt.plot([act[i] for act in actuator_history], label=f'Actuator {i+1}')
+plt.title("Actuator Outputs over Time")
+plt.ylabel("Actuator Output")
+plt.legend()
+
+# Torque
+plt.subplot(7, 2, 12)
+torque_labels = ['Roll', 'Pitch', 'Yaw']
+for i in range(3):
+    plt.plot([t[1][i] for t in force_history], label=f'Torque {torque_labels[i]}')
+plt.title("Torques over Time")
+plt.ylabel("Torque")
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+save_flight_data_to_csv(history)
